596650567d1251ebfbd8d1ebb9dff4bff868513d
Kevin Sanchez <115368041+KevinSanchez96@users.noreply.github.com>
2026-04-23 20:21:19 -0300

    Modificar gds

---
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func generate_block():
+	var block_scene = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
+	var block_instance = block_scene.instantiate()
+	add_child(block_instance)
+	
+func _on_button_down() -> void:
+	generate_block()
